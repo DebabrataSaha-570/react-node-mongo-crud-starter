c313c68f9ed77d636c3816c67f207307e40820c9
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-11-19 08:45:02 +0600

    loaded database data and show on the website

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Users = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+
+    }, [])
     return (
         <div>
-            <h2>This is Users</h2>
+            <h2>Users Available : {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li key={user._id}> {user.name} :: {user.email}
+                        <button>Update</button>
+                        <button>X</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
