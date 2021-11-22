717fdfc2d2f260308dadf393faf8b31c39bfe07b
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-11-22 09:35:35 +0600

    seted update form to update an user

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -15,9 +15,33 @@ const UpdateUser = () => {
                 setUser(data)
             })
     }, [])
+    //update user 
+    const handleNameChange = (e) => {
+        console.log(e.target.value)
+        const updatedName = e.target.value;
+        const updatedUser = { name: updatedName, email: user.email }
+        setUser(updatedUser)
+
+
+
+    }
+    const handleEmailChange = (e) => {
+        const updatedEmail = e.target.value;
+        const updatedUser = { ...user }
+        updatedUser.email = updatedEmail;
+        setUser(updatedUser)
+    }
+    const handleUpdateUser = () => {
+
+    }
     return (
         <div>
-            <h2>Update User : {user.name}</h2>
+            <h2>Update User : {user.name} || {user.email}</h2>
+            <form onSubmit={handleUpdateUser}>
+                <input type="text" onChange={handleNameChange} value={user.name || ''} />
+                <input type="email" onChange={handleEmailChange} value={user.email || ''} name="" id="" />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
